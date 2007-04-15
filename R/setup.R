603589f81b94c1c33faa9daa8e3961f080a4c8fe
sethf <sethf@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2007-04-15 00:46:01 +0000

    Remove setup.R, this code is not needed
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi/trunk@288 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- R/setup.R
+++
@@ -1,7 +0,0 @@
-## 
-## $Id$ 
-## the following is only needed during package installation,
-## .First.lib() takes care during attaching....
-.conflicts.OK <- TRUE
-require(methods, quietly = TRUE, warn.conflicts = FALSE)
-
