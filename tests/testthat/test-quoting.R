ed292152fccf999c97d58e410fd4447c50f5a830
hadley <h.wickham@gmail.com>
2015-01-29 07:44:53 -0600

    Testing quoting behaviour. More docs

---
+++ tests/testthat/test-quoting.R
@@ -0,0 +1,17 @@
+context("Quoting")
+
+test_that("NA becomes NULL", {
+  expect_equal(dbQuoteString(ANSI(), NA_character_), SQL("NULL"))
+})
+
+test_that("strings surrounded by '", {
+  expect_equal(dbQuoteString(ANSI(), "x"), SQL("'x'"))
+})
+
+test_that("single quotes are doubled surrounded by '", {
+  expect_equal(dbQuoteString(ANSI(), "It's"), SQL("'It''s'"))
+})
+
+test_that("special chars are escaped", {
+  expect_equal(dbQuoteString(ANSI(), "\n"), SQL("'\\n'"))
+})
