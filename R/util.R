626de59a26b663722c01402c19e0aae7e85d22ba
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-31 11:04:30 +0200

    remove print.list.pairs

--- R/util.R
+++ R/util.R
@@ -1,21 +1,3 @@
-#' Print a list of pairs.
-#'
-#' @param x a list of key, value pairs
-#' @param ... additional arguments to be passed to \code{cat}
-#' @return the (invisible) value of x.
-#' @keywords internal
-#' @export print.list.pairs
-#' @examples
-#' print.list.pairs(list(a = 1, b = 2))
-print.list.pairs <- function(x, ...) {
-  for(key in names(x)){
-    value <- format(x[[key]])
-    if (identical(value, "")) next
-    cat(key, "=", value, "\n")
-  }
-  invisible(x)
-}
-
 # Give a deprecation error, warning, or messsage, depending on version number.
 dbi_dep <- function(version, msg) {
   v <- as.package_version(version)
