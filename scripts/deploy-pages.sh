9a4318e478c004501f8853253a3213cee7d68105
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-08 22:44:53 +0200

    deploy-pages script won't set global user name and e-mail anymore

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -12,20 +12,28 @@ doc_dir=inst/web
 # TRAVIS_OS_NAME=linux TRAVIS_PULL_REQUEST=false TRAVIS_BRANCH=master TRAVIS_REPO_SLUG=rstats-db/DBI TRAVIS_COMMIT=$(git rev-parse HEAD) GITHUB_PAT=<your-PAT> scripts/deploy-pages.sh
 
 if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "production" ] && [ "$GITHUB_PAT" != "" ]; then
+  # Query name and e-mail of current author
+  # https://gist.github.com/l15n/3103708
+  user_email=$(git show --format="%ae" | head -n 1)
+  user_name=$(git show --format="%an" | head -n 1)
+
   rm -rf $doc_dir
   mkdir -p $doc_dir
 
-  # We'll pretend to be a new user. Clone the current docs.
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
+  # Clone the current docs.
   git clone --quiet --branch=gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG} $doc_dir > /dev/null
 
+  # Clean current docs and build anew.
   echo -e "Building staticdocs...\n"
   rm -r $doc_dir/*
   R -q -e "staticdocs::build_site()"
 
   echo -e "Publishing staticdocs...\n"
   cd $doc_dir
+
+  # Reuse the name and e-mail of the author of the last commit
+  git config user.email $user_email
+  git config user.name $user_name
 
   # Include .nojekyll as a directive for github
   touch .nojekyll
