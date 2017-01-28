a6250a1cf10fbe7b8a461341b47427bb483224e2
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-28 15:44:34 +0100

    add spec for dbQuoteString()

--- R/quote.R
+++ R/quote.R
@@ -5,10 +5,10 @@ NULL
 #'
 #' This set of classes and generics make it possible to flexibly deal with SQL
 #' escaping needs. By default, any user supplied input to a query should be
-#' escaped using either `dbQuoteIdentifier` or `dbQuoteString`
+#' escaped using either `dbQuoteIdentifier` or [dbQuoteString()]
 #' depending on whether it refers to a table or variable name, or is a literal
 #' string.
-#' These functions return an object of the `SQL` class,
+#' These functions may return an object of the `SQL` class,
 #' which tells DBI functions that a character string does not need to be escaped
 #' anymore, to prevent double escaping.
 #' The `SQL` class has associated the `SQL()` constructor function.
@@ -22,8 +22,6 @@ NULL
 #' `dbQuoteString(MyConnection, character)`, you will also need to
 #' implement `dbQuoteString(MyConnection, SQL)` - this should simply
 #' return `x` unchanged.
-#'
-#' If you implement your own method, make sure to convert NA to NULL (unquoted).
 #'
 #' @param conn A subclass of \code{\linkS4class{DBIConnection}}, representing
 #'   an active connection to an DBMS.
@@ -106,9 +104,32 @@ setMethod("dbQuoteIdentifier", c("DBIConnection", "SQL"),
   }
 )
 
-#' @rdname SQL
+#' Quote literal strings
+#'
+#' Call this function to generate a string that is suitable for
+#' use in a query as a string literal, to make sure that you
+#' generate valid SQL and avoid SQL injection.
+#'
+#' @inherit DBItest::spec_sql_quote_string return
+#' @inheritSection DBItest::spec_sql_quote_string Specification
+#'
 #' @family DBIResult generics
 #' @export
+#' @examples
+#' # Quoting ensures that arbitrary input is safe for use in a query
+#' name <- "Robert'); DROP TABLE Students;--"
+#' dbQuoteString(ANSI(), name)
+#'
+#' # NAs become NULL
+#' dbQuoteString(ANSI(), c("x", NA))
+#'
+#' # SQL vectors are always passed through as is
+#' var_name <- SQL("select")
+#' var_name
+#' dbQuoteString(ANSI(), var_name)
+#'
+#' # This mechanism is used to prevent double escaping
+#' dbQuoteString(ANSI(), dbQuoteString(ANSI(), name))
 setGeneric(
   "dbQuoteString",
   function(conn, x, ...) standardGeneric("dbQuoteString")
