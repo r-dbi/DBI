97a7d75808c171d956529fd0cfc3328d5acd4731
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-20 14:10:15 +0100

    fix pkgdown

--- tic.R
+++ tic.R
@@ -6,7 +6,7 @@ get_stage("deploy") %>%
   add_step(step_install_ssh_keys()) %>%
   add_step(step_test_ssh())
 
-if (ci()$get_branch() == "production" && Sys.getenv("BUILD_PKGDOWN") != "") {
+if (ci()$get_branch() == "master" && Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
