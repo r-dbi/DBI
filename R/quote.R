cd4ad421a93af8aa2cb54fe3d96f63fc4baf65c0
Kirill Müller <krlmlr+r@mailbox.org>
2018-03-27 13:29:25 +0200

    support unquoting simple identifiers

--- R/quote.R
+++ R/quote.R
@@ -197,17 +197,18 @@ setGeneric("dbUnquoteIdentifier",
 #' @export
 setMethod("dbUnquoteIdentifier", signature("DBIConnection"), function(conn, x, ...) {
   if (is(x, "SQL")) {
-    rx <- '^(?:|`((?:[^`]|``)+)`[.])(?:|`((?:[^`]|``)*)`)$'
+    rx <- '^(?:(?:|`((?:[^`]|``)+)`[.])(?:|`((?:[^`]|``)*)`)|([^`. ]+))$'
     bad <- grep(rx, x, invert = TRUE)
     if (length(bad) > 0) {
       stop("Can't unquote ", x[bad[[1]]], call. = FALSE)
     }
     schema <- gsub(rx, "\\1", x)
-    schema <- gsub('""', '"', schema)
+    schema <- gsub('``', '`', schema)
     table <- gsub(rx, "\\2", x)
-    table <- gsub('""', '"', table)
+    table <- gsub('``', '`', table)
+    naked_table <- gsub(rx, "\\3", x)
 
-    ret <- Map(schema, table, f = as_table)
+    ret <- Map(schema, table, naked_table, f = as_table)
     names(ret) <- names(x)
     return(ret)
   }
@@ -217,8 +218,8 @@ setMethod("dbUnquoteIdentifier", signature("DBIConnection"), function(conn, x, .
   stop("x must be SQL or Id", call. = FALSE)
 })
 
-as_table <- function(schema, table) {
-  args <- c(schema = schema, table = table)
+as_table <- function(schema, table, naked_table = NULL) {
+  args <- c(schema = schema, table = table, table = naked_table)
   # Also omits NA args
   args <- args[!is.na(args) & args != ""]
   do.call(Id, as.list(args))
