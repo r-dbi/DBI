f3342a0cb5bc6f5079246c0a0b88939a28a4ac27
Juergen Altfeld <R@altfeld-im.de>
2018-05-01 12:10:56 +0200

    fixes #239 (update docs on how to cope with stored procedures)

--- R/deprecated.R
+++ R/deprecated.R
@@ -199,7 +199,14 @@ setMethod("SQLKeywords", signature("missing"),
 
 #' Call an SQL stored procedure
 #'
-#' DEPRECATED
+#' **Deprecated since 2014**
+#'
+#' The recommended way of calling a stored procedure is now
+#'
+#' \enumerate{
+#' \item{\code{\link{dbGetQuery}} if a result set is returned}
+#' \item{\code{\link{dbExecute}}  for data manipulation and other cases where no result set is returned}
+#' }
 #'
 #' @inheritParams dbGetQuery
 #' @keywords internal
