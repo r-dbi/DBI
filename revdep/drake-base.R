1be0b47fba304cfac8ce1077f4b23753539327a4
Kirill Müller <krlmlr+r@mailbox.org>
2018-05-01 10:42:52 +0200

    up revdep code from pillar

--- revdep/drake-base.R
+++ revdep/drake-base.R
@@ -2,7 +2,7 @@ library(drake)
 library(tidyverse)
 library(glue)
 
-options(repos = revdepcheck:::get_repos(TRUE))
+options(repos = revdepcheck:::get_repos(FALSE))
 
 get_this_pkg <- function() {
   desc::desc_get("Package") %>% unname()
@@ -26,13 +26,17 @@ retry <- function(code, N = 1) {
   stop(ret)
 }
 
-plan_deps <- drake_plan(
-  available = available.packages(),
-  this_pkg = get_this_pkg(),
-  revdeps = tools::package_dependencies(this_pkg, available, 'most', reverse = TRUE) %>% flatten(),
-  first_level_deps = tools::package_dependencies(revdeps, available, 'most'),
-  all_level_deps = tools::package_dependencies(first_level_deps %>% flatten(), available, recursive = TRUE),
-  base_pkgs = get_base_pkgs(),
-  deps = c(revdeps, first_level_deps, all_level_deps) %>% flatten() %>% tools::package_dependencies(recursive = TRUE) %>% .[!(names(.) %in% base_pkgs)],
-  strings_in_dots = "literals"
-)
+get_plan_deps <- function() {
+  plan_deps <- drake_plan(
+    available = available.packages(),
+    this_pkg = get_this_pkg(),
+    revdeps = tools::package_dependencies(this_pkg, available, 'most', reverse = TRUE) %>% flatten(),
+    first_level_deps = tools::package_dependencies(revdeps, available, 'most'),
+    all_level_deps = tools::package_dependencies(first_level_deps %>% flatten(), available, recursive = TRUE),
+    base_pkgs = get_base_pkgs(),
+    deps = c(revdeps, first_level_deps, all_level_deps) %>% flatten() %>% tools::package_dependencies(recursive = TRUE) %>% .[!(names(.) %in% base_pkgs)],
+    strings_in_dots = "literals"
+  )
+
+  plan_deps
+}
