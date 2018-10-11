e20d29bce28ee1ba004f4fc508f63bdd4e1c7b7a
Sean Raffuse <raffscallion@gmail.com>
2018-10-11 09:21:38 -0700

    Add tests for dbUnquoteIdentifier

--- tests/testthat/test-quote.R
+++ tests/testthat/test-quote.R
@@ -23,3 +23,22 @@ test_that("SQL names", {
   expect_equal(names(SQL(letters, names = LETTERS)), LETTERS)
   expect_null(names(SQL(setNames(letters, LETTERS))))
 })
+
+test_that("unquote Id", {
+  expect_equal(dbUnquoteIdentifier(ANSI(), Id(table = "a")), list(Id(table = "a")))
+  expect_equal(dbUnquoteIdentifier(ANSI(), Id(table = "a", schema = "b")), list(Id(table = "a", schema = "b")))
+  expect_equal(dbUnquoteIdentifier(ANSI(), Id(table = "a", schema = "b", catalog = "c")),
+               list(Id(table = "a", schema = "b", catalog = "c")))
+})
+
+test_that("unquote SQL", {
+  expect_equal(dbUnquoteIdentifier(ANSI(), SQL("a")), list(Id(table = "a")))
+  expect_equal(dbUnquoteIdentifier(ANSI(), SQL('"b"."a"')), list(Id(schema = "b", table = "a")))
+  expect_equal(dbUnquoteIdentifier(ANSI(), SQL('"c"."b"."a"')),
+               list(Id(catalog = "c", schema = "b", table = "a")))
+  expect_equal(dbUnquoteIdentifier(ANSI(), SQL('"c"."b"."a"')),
+               list(Id(catalog = "c", schema = "b", table = "a")))
+  expect_equal(dbUnquoteIdentifier(ANSI(), SQL(c('"Catalog"."Schema"."Table"', '"UnqualifiedTable"'))),
+               list(Id(catalog = "Catalog", schema = "Schema", table = "Table"),
+                    Id(table = "UnqualifiedTable")))
+})
