d78f5d65db0b758596136737960112772477971a
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-21 23:04:52 +0200

    Support R >= 3.2 in tests

--- tic.R
+++ tic.R
@@ -1,20 +1,10 @@
-add_package_checks()
+do_package_checks()
 
-if (Sys.getenv("BUILD_PKGDOWN") != "" && !ci()$is_tag()) {
-  get_stage("deploy") %>%
-    add_code_step(prepare_call = remotes::install_github(c("r-lib/pkgdown", "r-lib/pkgapi"))) %>%
-    add_step(step_build_pkgdown())
+if (ci_has_env("DEV_VERSIONS")) {
+  get_stage("install") %>%
+    add_step(step_install_github(c("r-dbi/DBI", "r-dbi/RSQLite", "r-dbi/RPostgres", "r-dbi/RMariaDB")))
+}
 
-  if (Sys.getenv("id_rsa") != "") {
-    get_stage("before_deploy") %>%
-      add_step(step_setup_ssh())
-
-    # pkgdown documentation can be built optionally. Other example criteria:
-    # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-    # - `ci()$is_tag()`: Only for tags, not for branches
-    # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-    # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-    get_stage("deploy") %>%
-      add_step(step_push_deploy(commit_paths = "docs"))
-  }
+if (ci_has_env("BUILD_PKGDOWN") && !ci_is_tag()) {
+  do_pkgdown()
 }
