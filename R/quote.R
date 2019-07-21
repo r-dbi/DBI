c00d82967ecfacf7b5d1d97cdd39dde699ac112d
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-21 15:25:41 +0200

    - `dbUnquoteIdentifier()` also handles unquoted identifiers of the form `table`, `schema.table` or `catalog.schema.table`, for compatibility with dbplyr.

--- R/quote.R
+++ R/quote.R
@@ -197,20 +197,28 @@ setGeneric("dbUnquoteIdentifier",
 #' @export
 setMethod("dbUnquoteIdentifier", signature("DBIConnection"), function(conn, x, ...) {
   if (is(x, "SQL")) {
-    rx <- '^(?:(?:|"((?:[^"]|"")+)"[.])(?:|"((?:[^"]|"")+)"[.])(?:|"((?:[^"]|"")*)")|([^". ]+))$'
+    id_rx <- '(?:"((?:[^"]|"")+)"|([^". ]+))'
+
+    rx <- paste0(
+      "^",
+      "(?:|(?:|", id_rx, "[.])",
+      id_rx, "[.])",
+      "(?:|", id_rx, ")",
+      "$"
+    )
+
     bad <- grep(rx, x, invert = TRUE)
     if (length(bad) > 0) {
       stop("Can't unquote ", x[bad[[1]]], call. = FALSE)
     }
-    catalog <- gsub(rx, "\\1", x)
+    catalog <- gsub(rx, "\\1\\2", x)
     catalog <- gsub('""', '"', catalog)
-    schema <- gsub(rx, "\\2", x)
+    schema <- gsub(rx, "\\3\\4", x)
     schema <- gsub('""', '"', schema)
-    table <- gsub(rx, "\\3", x)
+    table <- gsub(rx, "\\5\\6", x)
     table <- gsub('""', '"', table)
-    naked_table <- gsub(rx, "\\4", x)
 
-    ret <- Map(catalog, schema, table, naked_table, f = as_table)
+    ret <- Map(catalog, schema, table, f = as_table)
     names(ret) <- names(x)
     return(ret)
   }
@@ -220,8 +228,8 @@ setMethod("dbUnquoteIdentifier", signature("DBIConnection"), function(conn, x, .
   stop("x must be SQL or Id", call. = FALSE)
 })
 
-as_table <- function(catalog, schema, table, naked_table = NULL) {
-  args <- c(catalog = catalog, schema = schema, table = table, table = naked_table)
+as_table <- function(catalog, schema, table) {
+  args <- c(catalog = catalog, schema = schema, table = table)
   # Also omits NA args
   args <- args[!is.na(args) & args != ""]
   do.call(Id, as.list(args))
