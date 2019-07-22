042efecff5690359d602a9868b067e44bb96f09a
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-22 10:11:43 +0200

    Fewer dev versions

--- tic.R
+++ tic.R
@@ -2,7 +2,7 @@ do_package_checks()
 
 if (ci_has_env("DEV_VERSIONS")) {
   get_stage("install") %>%
-    add_step(step_install_github(c("r-dbi/DBI", "r-dbi/RSQLite", "r-dbi/RPostgres", "r-dbi/RMariaDB")))
+    add_step(step_install_github(c("r-dbi/DBI", "r-dbi/RSQLite")))
 }
 
 if (ci_has_env("BUILD_PKGDOWN") && !ci_is_tag()) {
