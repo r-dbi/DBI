17414576fc8e42a3b8d61c89e38e1f855e984e43
Kirill Müller <krlmlr+r@mailbox.org>
2019-10-13 12:49:46 +0200

    - Update examples to refer to `params` instead of `param` (#235).

--- R/DBConnection.R
+++ R/DBConnection.R
@@ -117,7 +117,7 @@ setGeneric("dbDisconnect",
 #' rs <- dbSendQuery(
 #'   con,
 #'   "SELECT * FROM mtcars WHERE cyl = ?",
-#'   param = list(4L)
+#'   params = list(4L)
 #' )
 #' dbFetch(rs)
 #' dbClearResult(rs)
@@ -182,7 +182,7 @@ setGeneric("dbSendQuery",
 #' rs <- dbSendStatement(
 #'   con,
 #'   "INSERT INTO cars (speed, dist) VALUES (?, ?)",
-#'   param = list(4L, 5L)
+#'   params = list(4L, 5L)
 #' )
 #' dbClearResult(rs)
 #'
@@ -262,7 +262,11 @@ setMethod(
 #' # (This query runs eight times, once for each different
 #' # parameter. The resulting rows are combined into a single
 #' # data frame.)
-#' dbGetQuery(con, "SELECT COUNT(*) FROM mtcars WHERE cyl = ?", param = list(1:8))
+#' dbGetQuery(
+#'   con,
+#'   "SELECT COUNT(*) FROM mtcars WHERE cyl = ?",
+#'   params = list(1:8)
+#' )
 #'
 #' dbDisconnect(con)
 setGeneric("dbGetQuery",
@@ -325,7 +329,7 @@ setMethod("dbGetQuery", signature("DBIConnection", "character"),
 #' dbExecute(
 #'   con,
 #'   "INSERT INTO cars (speed, dist) VALUES (?, ?)",
-#'   param = list(4:7, 5:8)
+#'   params = list(4:7, 5:8)
 #' )
 #' dbReadTable(con, "cars")   # there are now 10 rows
 #'
