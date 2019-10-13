bc6735741bad4f1d33ae203de97008e7683516ab
Kirill Müller <krlmlr+r@mailbox.org>
2019-10-13 12:32:27 +0200

    Use dev rmarkdown to work around article problem

--- tic.R
+++ tic.R
@@ -1,3 +1,7 @@
+# https://github.com/r-lib/pkgdown/issues/1163
+get_stage("install") %>%
+  add_step(step_install_github(c("rstudio/rmarkdown")))
+
 do_package_checks()
 
 if (ci_has_env("DEV_VERSIONS")) {
