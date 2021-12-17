b061dbc8a3600e544745f573094d91bbb4ad257e
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-17 17:21:40 +0100

    Move away

--- R/ANSI.R
+++ R/ANSI.R
@@ -1,6 +1,6 @@
 #' @include hidden.R
 #' @include DBConnection.R
-#' @include DBDriver.R
+#' @include DBDriver_.R
 setClass("AnsiConnection", contains = "DBIConnection")
 
 #' A dummy DBI connector that simulates ANSI-SQL compliance
