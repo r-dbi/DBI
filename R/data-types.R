5316ba1412f0b8d8d9134eff2216bafb8d15a8bb
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-17 17:14:25 +0100

    Manual split

--- R/data-types.R
+++ R/data-types.R
@@ -1,17 +1,5 @@
 #' @include dbiDataType.R
 
-list_data_type <- function(x) {
-  check_raw_list(x)
-  "BLOB"
-}
-
-check_raw_list <- function(x) {
-  is_raw <- vapply(x, is.raw, logical(1))
-  if (!all(is_raw)) {
-    stop("Only lists of raw vectors are currently supported", call. = FALSE)
-  }
-}
-
 as_is_data_type <- function(x) {
   oldClass(x) <- oldClass(x)[-1]
   dbiDataType(x)
@@ -21,7 +9,6 @@ setOldClass("difftime")
 
 setOldClass("AsIs")
 
-setMethod("dbiDataType", signature("list"),       list_data_type)
 
 setMethod("dbiDataType", signature("AsIs"),       as_is_data_type)
 
