561ffa39c9b41585e1dbac65d0496d7233bcbb2c
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-17 17:15:37 +0100

    Manual split

--- R/data-types.R
+++ R/data-types.R
@@ -1,14 +1,3 @@
-#' @include dbiDataType.R
-
-as_is_data_type <- function(x) {
-  oldClass(x) <- oldClass(x)[-1]
-  dbiDataType(x)
-}
-
 setOldClass("difftime")
 
 setOldClass("AsIs")
-
-
-setMethod("dbiDataType", signature("AsIs"),       as_is_data_type)
-
