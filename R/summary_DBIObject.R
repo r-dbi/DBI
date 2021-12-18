35aaab0739ae9d933c914a5158c25b8d503593ba
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-18 06:31:07 +0100

    Join

--- R/summary_DBIObject.R
+++ R/summary_DBIObject.R
@@ -7,3 +7,12 @@ summary_DBIObject <- function(object, ...) {
 }
 
 setMethod("summary", "DBIObject", summary_DBIObject)
+
+print_list_pairs <- function(x, ...) {
+  for (key in names(x)) {
+    value <- format(x[[key]])
+    if (identical(value, "")) next
+    cat(key, "=", value, "\n")
+  }
+  invisible(x)
+}
