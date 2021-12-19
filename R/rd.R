88ed26b8523fa637bea58bd1cee7e288ee132d95
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-19 10:02:53 +0100

    Autolink

--- R/rd.R
+++ R/rd.R
@@ -14,13 +14,17 @@ methods_as_rd <- function(method) {
       error = function(e) NA
     )
     if (is.na(package) || package == "DBI") {
-      NA_character_
+      return(NA_character_)
+    }
+
+    topic <- paste0(x@generic, ",", sig, "-method")
+    call <- paste0(package, "::", x@generic, "(", sig_text, ")")
+
+    if (identical(Sys.getenv("IN_PKGDOWN"), "true")) {
+      url <- downlit::autolink_url(paste0("?", package, "::`", topic, "`"))
+      paste0("\\code{\\href{", url , "}{", call, "}}")
     } else {
-      paste0(
-        "\\code{\\link[=", x@generic, ",", sig, "-method]{",
-        package, "::", x@generic,
-        "(", sig_text, ")}}"
-      )
+      paste0("\\code{\\link[=", topic, "]{", call, "}}")
     }
   }
 
