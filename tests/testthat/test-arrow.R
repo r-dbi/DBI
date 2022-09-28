dbe69059be01efce0602890fb7c43eaf927a2966
Kirill Müller <krlmlr+r@mailbox.org>
2022-09-28 11:43:40 +0200

    Use custom data

--- tests/testthat/test-arrow.R
+++ tests/testthat/test-arrow.R
@@ -7,26 +7,33 @@ test_that("write arrow to sqlite", {
   con <- dbConnect(RSQLite::SQLite(), ":memory:")
   on.exit(dbDisconnect(con))
 
-  tbl <- arrow::as_arrow_table(mtcars)
+  data <- data.frame(
+    a = 1L,
+    b = 2.5,
+    c = "three",
+    stringsAsFactors = FALSE
+  )
 
-  res <- dbWriteStream(con, "mtcars_tbl", tbl)
+  tbl <- arrow::as_arrow_table(data)
+
+  res <- dbWriteStream(con, "data_tbl", tbl)
 
   expect_equal(
-    dbReadTable(con, "mtcars_tbl"),
+    dbReadTable(con, "data_tbl"),
     as.data.frame(tbl)
   )
 
   expect_equal(
-    dbStreamTable(con, "mtcars_tbl")$read_table(),
+    dbStreamTable(con, "data_tbl")$read_table(),
     tbl
   )
 
   expect_equal(
-    as.data.frame(dbGetStream(con, "SELECT COUNT(*) FROM mtcars_tbl")$read_table())[[1]],
+    as.data.frame(dbGetStream(con, "SELECT COUNT(*) FROM data_tbl")$read_table())[[1]],
     nrow(tbl)
   )
 
-  res <- dbSendQueryArrow(con, "SELECT COUNT(*) FROM mtcars_tbl")
+  res <- dbSendQueryArrow(con, "SELECT COUNT(*) FROM data_tbl")
   expect_equal(
     as.data.frame(dbStream(res)$read_table())[[1]],
     nrow(tbl)
