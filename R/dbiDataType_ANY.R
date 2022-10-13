3ba39e7779006f408bc0bd41acdd82a79842c836
Kirill Müller <krlmlr+r@mailbox.org>
2022-10-13 04:52:31 +0200

    feat: Implement `dbiDataType()` for objects of class `"blob"`

---
+++ R/dbiDataType_ANY.R
@@ -0,0 +1,8 @@
+dbiDataType_ANY <- function(x) {
+  if (inherits(x, "blob")) {
+    return("BLOB")
+  }
+  stop("Unknown SQL data type for object of class ", paste(class(x), collapse = "/"))
+}
+
+setMethod("dbiDataType", signature("ANY"), dbiDataType_ANY)
