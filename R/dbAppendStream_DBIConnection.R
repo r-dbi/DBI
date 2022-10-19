f1a3f2514aa26b68c7000474f1c9045e0f1abebb
Kirill Müller <krlmlr+r@mailbox.org>
2022-10-19 04:26:41 +0200

    feat: `dbAppendStream()` silences warnings about converting factors to character

--- R/dbAppendStream_DBIConnection.R
+++ R/dbAppendStream_DBIConnection.R
@@ -13,7 +13,7 @@ dbAppendStream_DBIConnection <- function(conn, name, value, ...) {
     if (is.null(tmp)) {
       break
     }
-    dbAppendTable(conn, name, as.data.frame(tmp), ...)
+    dbAppendTable(conn, name, stream_append_data(as.data.frame(tmp)), ...)
   }
 
   TRUE
@@ -21,3 +21,41 @@ dbAppendStream_DBIConnection <- function(conn, name, value, ...) {
 #' @rdname hidden_aliases
 #' @export
 setMethod("dbAppendStream", signature("DBIConnection"), dbAppendStream_DBIConnection)
+
+stream_append_data <- function(value) {
+  value <- factor_to_string(value)
+  value <- string_to_utf8(value)
+  value
+}
+
+factor_to_string <- function(value, warn = FALSE) {
+  is_factor <- vlapply(value, is.factor)
+  if (warn && any(is_factor)) {
+    warning("Factors converted to character", call. = FALSE)
+  }
+  value[is_factor] <- lapply(value[is_factor], as.character)
+  value
+}
+
+raw_to_string <- function(value) {
+  is_raw <- vlapply(value, is.raw)
+
+  if (any(is_raw)) {
+    warning("Creating a TEXT column from raw, use lists of raw to create BLOB columns", call. = FALSE)
+    value[is_raw] <- lapply(value[is_raw], as.character)
+  }
+
+  value
+}
+
+quote_string <- function(value, conn) {
+  is_character <- vlapply(value, is.character)
+  value[is_character] <- lapply(value[is_character], dbQuoteString, conn = conn)
+  value
+}
+
+string_to_utf8 <- function(value) {
+  is_char <- vlapply(value, is.character)
+  value[is_char] <- lapply(value[is_char], enc2utf8)
+  value
+}
