11f520920bec7ec44cfff2a5e48ecaac054551a8
Kirill Müller <kirill@cynkra.com>
2023-12-23 12:53:52 +0100

    dbCreateTableArrow

--- R/22-dbCreateTableArrow.R
+++ R/22-dbCreateTableArrow.R
@@ -1,7 +1,37 @@
-#' @name dbCreateTable
-#' @aliases dbCreateTableArrow
-#' @param value An object coercible to an Arrow RecordBatchReader.
+#' Create a table in the database based on an Arrow object
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#' The default `dbCreateTableArrow()` method determines the R data types
+#' of the Arrow schema associated with the Arrow object,
+#' and calls [dbCreateTable()].
+#' Backends that implement [dbAppendTableArrow()] should typically
+#' also implement this generic.
+#' Use [dbCreateTable()] to create a table from the column types
+#' as defined in a data frame.
+#'
+#' @param value An object for which a schema can be determined via
+#'   [nanoarrow::infer_nanoarrow_schema()].
+#' @inheritParams dbReadTable
+#' @inheritParams sqlCreateTable
+#'
+#' @inherit DBItest::spec_arrow_create_table_arrow return
+#' @inheritSection DBItest::spec_arrow_create_table_arrow Failure modes
+#' @inheritSection DBItest::spec_arrow_create_table_arrow Additional arguments
+#' @inheritSection DBItest::spec_arrow_create_table_arrow Specification
+#'
+#' @template methods
+#' @templateVar method_name dbCreateTableArrow
+#'
+#' @family DBIConnection generics
 #' @export
+#' @examplesIf requireNamespace("RSQLite", quietly = TRUE) && requireNamespace("nanoarrow", quietly = TRUE)
+#' con <- dbConnect(RSQLite::SQLite(), ":memory:")
+#' ptype <- data.frame(a = numeric())
+#' dbCreateTableArrow(con, "df", nanoarrow::infer_nanoarrow_schema(ptype))
+#' dbReadTable(con, "df")
+#' dbDisconnect(con)
 setGeneric("dbCreateTableArrow",
   def = function(conn, name, value, ...) standardGeneric("dbCreateTableArrow")
 )
